5b8f1229867efa9282e98baab9d63e7f89ff8d96
_ Trentini _ <pietrotrentini5@gmail.com>
2020-10-13 09:21:33 +0200

    Scrittura del codice della prima versione

--- conversioneDecimaleBinario/Program.cs
+++ conversioneDecimaleBinario/Program.cs
@@ -6,7 +6,19 @@ namespace conversioneDecimaleBinario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num, resto;
+            Console.Write("inserisci un numero da convertitre: ");
+            num = int.Parse(Console.ReadLine());
+            string binario = "";
+            while (num > 0)
+            {
+                resto = num % 2;
+                num /= 2;
+                binario = Convert.ToString(resto) + binario;
+
+            }
+            Console.WriteLine($"il numero in binario è {binario}");
+            Console.ReadLine();
         }
     }
 }
